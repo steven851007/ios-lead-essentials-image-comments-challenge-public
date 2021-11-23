92f231e371877e2901b2933b1547874b42bc558c
Istvan <istvan.balogh@outlook.com>
2021-11-23 14:18:57 +0700

    Create UI classes

---
+++ EssentialFeed/EssentialFeediOSTests/ImageComments UI/ImageCommentsSnapshotTests.swift
@@ -0,0 +1,12 @@
+//	
+// Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import XCTest
+import EssentialFeediOS
+@testable import EssentialFeed
+
+class ImageCommentsSnapshotTests: XCTestCase {
+
+    
+}
