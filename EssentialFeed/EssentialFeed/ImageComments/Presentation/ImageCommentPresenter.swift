8b707f5fad7ceec5dd86971836616c9e92ebf573
Istvan <istvan.balogh@outlook.com>
2022-01-20 13:02:47 +0700

    Refactor `ImageCommentPresenterTests`

--- EssentialFeed/EssentialFeed/ImageComments/Presentation/ImageCommentPresenter.swift
+++ EssentialFeed/EssentialFeed/ImageComments/Presentation/ImageCommentPresenter.swift
@@ -5,8 +5,7 @@
 import Foundation
 
 public final class ImageCommentPresenter {
-	public static func map(_ imageComment: ImageComment) -> ImageCommentViewModel {
-		let formatter = RelativeDateTimeFormatter()
+	public static func map(_ imageComment: ImageComment, formatter: RelativeDateTimeFormatter = RelativeDateTimeFormatter()) -> ImageCommentViewModel {
 		let dateString = formatter.string(for: imageComment.createdAt)!
 		return ImageCommentViewModel(comment: imageComment.message, date: dateString, author: imageComment.userName)
 	}
--- EssentialFeed/EssentialFeedTests/ImageComments/Presentation/ImageCommentPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageComments/Presentation/ImageCommentPresenterTests.swift
@@ -7,17 +7,34 @@ import EssentialFeed
 
 class ImageCommentPresenterTests: XCTestCase {
 	func test_map_createsViewModel() {
-		let dateText = "21 years ago"
 		let imageComment = uniqueImageComment(date: Date(timeIntervalSinceReferenceDate: 0))
+		let mockFormatter = MockRelativeDateTimeFormatter(returnText: "21 years ago")
 
-		let viewModel = ImageCommentPresenter.map(imageComment)
+		let viewModel = ImageCommentPresenter.map(imageComment, formatter: mockFormatter)
 
 		XCTAssertEqual(viewModel.comment, imageComment.message)
-		XCTAssertEqual(viewModel.date, dateText)
+		XCTAssertEqual(viewModel.date, mockFormatter.returnText)
 		XCTAssertEqual(viewModel.author, imageComment.userName)
 	}
 
 	private func uniqueImageComment(date: Date) -> ImageComment {
 		return ImageComment(id: UUID(), message: "a message", createdAt: date, userName: "a user")
+	}
+
+	private class MockRelativeDateTimeFormatter: RelativeDateTimeFormatter {
+		var returnText: String?
+
+		init(returnText: String) {
+			self.returnText = returnText
+			super.init()
+		}
+
+		required init?(coder: NSCoder) {
+			fatalError("init(coder:) has not been implemented")
+		}
+
+		override func string(for obj: Any?) -> String? {
+			return returnText
+		}
 	}
 }
