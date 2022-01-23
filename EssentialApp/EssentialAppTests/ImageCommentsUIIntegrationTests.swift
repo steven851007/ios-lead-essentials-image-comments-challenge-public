bde48a41770b6b8ed7d29fcfc2537ccc6a052151
Istvan <istvan.balogh@outlook.com>
2022-01-23 09:16:06 +0700

    Add `test_imageCommentsDeinit_cancelsRunningRequest` test

--- EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/ImageCommentsUIIntegrationTests.swift
@@ -129,6 +129,29 @@ class ImageCommentsUIIntegrationTests: XCTestCase {
 		XCTAssertEqual(sut.errorMessage, nil)
 	}
 
+	func test_imageCommentsDeinit_cancelsRunningRequest() {
+		var cancelCallCount = 0
+
+		var sut: ListViewController?
+
+		autoreleasepool {
+			sut = CommentsUIComposer.commentsUIComposedWith(commentsLoader: {
+				PassthroughSubject<[ImageComment], Error>()
+					.handleEvents(receiveCancel: {
+						cancelCallCount += 1
+					}).eraseToAnyPublisher()
+			})
+
+			sut?.loadViewIfNeeded()
+		}
+
+		XCTAssertEqual(cancelCallCount, 0)
+
+		sut = nil
+
+		XCTAssertEqual(cancelCallCount, 1)
+	}
+
 	// MARK: - Helpers
 
 	private func makeSUT(
