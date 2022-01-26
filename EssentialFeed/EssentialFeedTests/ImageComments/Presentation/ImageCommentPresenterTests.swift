b088573d367f6f8cc899f22275bbe64e2ead5019
Istvan <istvan.balogh@outlook.com>
2022-01-26 10:59:00 +0700

    PR Feedback

--- EssentialFeed/EssentialFeedTests/ImageComments/Presentation/ImageCommentPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/ImageComments/Presentation/ImageCommentPresenterTests.swift
@@ -11,16 +11,21 @@ class ImageCommentPresenterTests: XCTestCase {
 		let calendar = Calendar(identifier: .gregorian)
 		let locale = Locale(identifier: "en_US_POSIX")
 
-		let imageComment = uniqueImageComment(date: Date().addingTimeInterval(-5 * 61))
+		let viewModels = [
+			uniqueImageComment(date: Date().addingTimeInterval(-5 * 61)),
+			uniqueImageComment("another message", date: Date().addingTimeInterval(-10 * 61), userName: "another user")
+		].map { ImageCommentPresenter.map($0, currentDate: now, calendar: calendar, locale: locale) }
 
-		let viewModel = ImageCommentPresenter.map(imageComment, currentDate: now, calendar: calendar, locale: locale)
+		XCTAssertEqual(viewModels.first?.comment, "a message")
+		XCTAssertEqual(viewModels.first?.date, "5 minutes ago")
+		XCTAssertEqual(viewModels.first?.author, "a user")
 
-		XCTAssertEqual(viewModel.comment, imageComment.message)
-		XCTAssertEqual(viewModel.date, "5 minutes ago")
-		XCTAssertEqual(viewModel.author, imageComment.userName)
+		XCTAssertEqual(viewModels.last?.comment, "another message")
+		XCTAssertEqual(viewModels.last?.date, "10 minutes ago")
+		XCTAssertEqual(viewModels.last?.author, "another user")
 	}
 
-	private func uniqueImageComment(date: Date) -> ImageComment {
-		return ImageComment(id: UUID(), message: "a message", createdAt: date, userName: "a user")
+	private func uniqueImageComment(_ message: String = "a message", date: Date, userName: String = "a user") -> ImageComment {
+		return ImageComment(id: UUID(), message: message, createdAt: date, userName: userName)
 	}
 }
--- EssentialFeed/EssentialFeediOSTests/ImageComments UI/ImageCommentsSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/ImageComments UI/ImageCommentsSnapshotTests.swift
@@ -10,7 +10,7 @@ class ImageCommentsSnapshotTests: XCTestCase {
 	func test_imageCommentsWithContent() {
 		let sut = makeSUT()
 
-		sut.display(imageCommentsContent())
+		sut.display(comments())
 
 		assert(snapshot: sut.snapshot(for: .iPhone13(style: .light)), named: "IMAGE_COMMENTS_WITH_CONTENT_light")
 		assert(snapshot: sut.snapshot(for: .iPhone13(style: .dark)), named: "IMAGE_COMMENTS_WITH_CONTENT_dark")
@@ -27,21 +27,18 @@ class ImageCommentsSnapshotTests: XCTestCase {
 		return controller
 	}
 
-	private func imageCommentsContent() -> [CellController] {
-		[longCommentCellController(), shortCommentCellController()]
-	}
-
-	private func shortCommentCellController() -> CellController {
-		let viewModel = ImageCommentViewModel(comment: "Short comment", date: "5 minutes ago", author: "This is a very long user name that might not fit into line one")
-
-		return CellController(id: UUID(), ImageCommentCellController(viewModel: viewModel))
-	}
-
-	private func longCommentCellController() -> CellController {
-		let message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet vitae lorem at placerat. Nam sollicitudin diam vel augue tincidunt consectetur. Pellentesque ultricies ligula ut ipsum ultricies fermentum. Pellentesque ut purus sit amet lacus egestas lobortis. Curabitur varius in leo quis laoreet. Sed ac sagittis sapien, viverra interdum enim.."
-
-		let viewModel = ImageCommentViewModel(comment: message, date: "2 hours ago", author: "a user")
-
-		return CellController(id: UUID(), ImageCommentCellController(viewModel: viewModel))
+	private func comments() -> [CellController] {
+		return [
+			ImageCommentViewModel(
+				comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquet vitae lorem at placerat. Nam sollicitudin diam vel augue tincidunt consectetur. Pellentesque ultricies ligula ut ipsum ultricies fermentum. Pellentesque ut purus sit amet lacus egestas lobortis. Curabitur varius in leo quis laoreet. Sed ac sagittis sapien, viverra interdum enim..",
+				date: "2 hours ago",
+				author: "a user"
+			),
+			ImageCommentViewModel(
+				comment: "Short comment",
+				date: "5 minutes ago",
+				author: "This is a very long user name that might not fit into line one"
+			)
+		].map({ CellController(id: UUID(), ImageCommentCellController(viewModel: $0)) })
 	}
 }
